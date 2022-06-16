6f9ebb7d4c9aa5ca0ace179effce6e116569fe97
SepevOleg <107648948+SepevOleg@users.noreply.github.com>
2022-06-16 23:11:08 +0300

    test

---
+++ js/header.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.menu_open').click(function () {
+        $('.nav').toggleClass('show-menu')
+    })
+
+});
+
+$(function() {
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+	});
+});
